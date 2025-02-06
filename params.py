13283249679334e9128990b30ba91eb242b1ddd4
Sanchir Ganzorig <sanchirganzorig@sanchirs-MacBook-Pro.local>
2025-02-06 17:06:51 +1100

    restructured sample files and hardcoded ids

--- params.py
+++ params.py
@@ -12,5 +12,3 @@ PROCESSED_FILE_TYPES = ['.cram', '.bam']
 SUMMARISED_FILE_TYPES = ['.vcf', '.maf']
 
 METADATA = "sampled_clinical_data.json"
-PATIENT_ID = "Patient ID" #what the Patient ID column is named in the file above
-SAMPLE_ID = "Sample ID" #What the Sample Id column is named in the file above
